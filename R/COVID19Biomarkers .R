e667b1447074e9a4c64538c4d2ca0a9e219f7614
shubham14101 <shubham14101@iiitd.ac.in>
2021-07-31 04:58:02 +0530

    package update

---
+++ R/COVID19Biomarkers .R
@@ -0,0 +1,3 @@
+ COVID19Biomarkers  <- function(){
+                                shiny::runApp(appDir = system.file('cd',package = 'COVID19Biomarkers'),launch.browser = TRUE)
+        }
--- R/COVID19_Biomarkers .R
+++
@@ -1,3 +0,0 @@
- COVID19_Biomarkers  <- function(){
-                                shiny::runApp(appDir = system.file('cd',package = 'COVID19_Biomarkers'),launch.browser = TRUE)
-        }
